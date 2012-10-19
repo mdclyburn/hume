a19b48cb7ca4cd0caef25f0721effdf64d3f7542
Marshall Clyburn <clyburnm@me.com>
2012-10-19 00:14:43 -0400

    Began Sprite class header.
    - This marks the creation of a new Sprite class to better manage the
      processing of sprite art.

---
+++ Sprite.h
@@ -0,0 +1,15 @@
+#ifndef SPRITE_H_INCLUDED
+#define SPRITE_H_INCLUDED
+
+#include "Image.h"
+
+namespace hm
+{
+    class Sprite : public hm::Image
+    {
+        public:
+        private:
+    }
+}
+
+#endif // SPRITE_H_INCLUDED
