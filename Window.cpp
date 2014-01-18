edccf779a0c3bbaad3224f6551e1051efad5ff5a
Marshall Clyburn <mardev2013@gmail.com>
2014-01-18 18:19:29 -0500

    Updated Window class.
    Window class is now ready for SDL2. A lot of functions have been
    reworked and removed from the class to allow it to be complete. There
    is a really kludgy implementation for knowing the width and height of the
    window object. As of now, it is only set when the constructor is used for
    the object and will not take resizing into account, which is potentially
    dangerous.
    
    Also, the Quick-Align functions have been removed until there is a way
    to retrieve the width and height of Blittable objects given to the Window.
    They will be back in a later commit.

--- Window.cpp
+++ Window.cpp
@@ -4,6 +4,8 @@ namespace hm
 {
 	Window::Window()
 	{
+		width = 640;
+		height = 480;
 		window = SDL_CreateWindow("No Title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 		if(window == nullptr)
 			std::cout << "SDL_Window creation failed.";
@@ -13,6 +15,8 @@ namespace hm
 
 	Window::Window(int w, int h)
 	{
+		width = w;
+		height = h;
 		window = SDL_CreateWindow("No Title", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, 0);
 		if(window == nullptr)
 			std::cout << "SDL_Window creation failed.";
@@ -22,6 +26,8 @@ namespace hm
 
 	Window::Window(std::string title, int w, int h)
 	{
+		width = w;
+		height = h;
 		window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, 0);
 		if(window == nullptr)
 			std::cout << "SDL_Window creation failed.";
@@ -34,15 +40,15 @@ namespace hm
 		// Nothing to do here.
 	}
 
-    	bool Window::getStatus()
-    	{
-        	return (screen != NULL);
-    	}
-
 	void Window::setTitle(std::string title)
 	{
-		SDL_WM_SetCaption(title.c_str(), NULL);
+		SDL_SetWindowTitle(window, title.c_str());
 		return;
+	}
+	
+	bool Window::getStatus()
+	{
+		return (window != nullptr);
 	}
 
 	void Window::draw(Blittable& b)
@@ -51,7 +57,7 @@ namespace hm
 		if(b.getAlpha() == TRANSPARENT)
 			return;
 		clear();
-		SDL_BlitSurface(b.getSurface(), NULL, screen, b.getPosition());
+		SDL_RenderCopy(renderer, b.getTexture(), nullptr, b.getPosition());
 		needRefresh = true;
 		return;
 	}
@@ -65,38 +71,15 @@ namespace hm
 		SDL_Rect sdlr;
 		sdlr.x = x;
 		sdlr.y = y;
-		SDL_BlitSurface(b.getSurface(), NULL, screen, &sdlr);
+		SDL_RenderCopy(renderer, b.getTexture(), nullptr, &sdlr);
+		return;
 	}
 	
 	void Window::clear()
 	{
 		// Don't clear the window if there are changes pending.
 		if(!needRefresh)
-			SDL_FillRect(screen, NULL, clearColor);
-		return;
-	}
-
-	Uint32 Window::getClearColor()
-	{
-		return clearColor;
-	}
-
-	void Window::setClearColor(Uint32 clearColor)
-	{
-		this->clearColor = clearColor;
-		return;
-	}
-
-	void Window::setClearColor(Uint8 red, Uint8 green, Uint8 blue)
-	{
-		clearColor = SDL_MapRGB(screen->format, red, green, blue);
-		return;
-	}
-
-	void Window::resetClearColor()
-	{
-		// Sets the clearColor to black.
-		clearColor = SDL_MapRGB(screen->format, 0x00, 0x00, 0x00);
+			SDL_RenderClear(renderer);
 		return;
 	}
 
@@ -105,7 +88,7 @@ namespace hm
 
 		if(needRefresh)
 		{
-			SDL_Flip(screen);
+			SDL_RenderPresent(renderer);
 			needRefresh = false;
 		}
 
@@ -114,77 +97,11 @@ namespace hm
 
 	int Window::getWidth()
 	{
-		return screen->w;
+		return width;
 	}
 
 	int Window::getHeight()
 	{
-		return screen->h;
-	}
-
-	// Centers the given Blittable in the given Window.
-	void Window::center(Blittable& b)
-	{
-		b.getPosition()->x = screen->w / 2 - b.getSurface()->w / 2;
-		b.getPosition()->y = screen->h / 2 - b.getSurface()->h / 2;
-
-		return;
-	}
-
-	// Centers the given Blittable on the X-Axis in the given window.
-	void Window::centerx(Blittable& b)
-	{
-		b.getPosition()->x = screen->w / 2 - b.getSurface()->w / 2;
-		return;
-	}
-
-	// Centers the given Blittable on the Y-Axis in the given window.
-	void Window::centery(Blittable& b)
-	{
-		b.getPosition()->y = screen->h / 2 - b.getSurface()->h / 2;
-		return;
-	}
-
-	void Window::centerxco(Blittable& b, int x)
-	{
-		b.getPosition()->x = x - b.getSurface()->w / 2;
-		return;
-	}
-
-	void Window::centeryco(Blittable& b, int y)
-	{
-		b.getPosition()->y = y - b.getSurface()->h / 2;
-		return;
-	}
-
-	void Window::centerco(Blittable& b, int x, int y)
-	{
-		centerxco(b, x);
-		centeryco(b, y);
-		return;
-	}
-
-	void Window::top(Blittable& b)
-	{
-		b.getPosition()->y = 0;
-		return;
-	}
-
-	void Window::bottom(Blittable& b)
-	{
-		b.getPosition()->y = screen->h - b.getSurface()->h;
-		return;
-	}
-
-	void Window::left(Blittable& b)
-	{
-		b.getPosition()->x = 0;
-		return;
-	}
-
-	void Window::right(Blittable& b)
-	{
-		b.getPosition()->x = screen->w - b.getSurface()->w;
-		return;
+		return height;
 	}
 }
--- Window.h
+++ Window.h
@@ -26,32 +26,16 @@ namespace hm
 		void draw(Blittable& b); // Draws a Blittable object to the window's screen.
 		void draw(Blittable& b, int x, int y); // Draws a Blittable object to the window's screen, overriding the position set internally.
 
-		// Clearing Functions
 		void clear(); // Blacks out the screen.
-		Uint32 getClearColor(); // Gets the currently used clear color.
-		void setClearColor(Uint32 clearColor); // Changes refresh color with unsigned 32-bit integer.
-		void setClearColor(Uint8 red, Uint8 green, Uint8 blue); // Changes refresh color with 8-bit integers.
-		void resetClearColor(); // Changes the clear color back to 255, 0, 255.
-		
 		void refresh(); // Updates the screen.
 
 		// Dimension Functions
 		int getWidth(); // Returns the width of the window in pixels.
 		int getHeight(); // Returns the height of the window in pixels.
 
-		// Quick-Align Functions
-		void center(Blittable& b); // Centers a blittable.
-		void centerx(Blittable& b); // Centers a blittable on the X-Axis.
-		void centery(Blittable& b); // Centers a blittable on the Y-Axis.
-		void centerxco(Blittable& b, int x); // Centers a blittable along a specified X-Coordinate.
-		void centeryco(Blittable& b, int y); // Centers a blittable along a specified Y-Coordinate.
-		void centerco(Blittable& b, int x, int y); // Centers a blittable at a specified point.
-		void top(Blittable& b); // Moves the blittable to the top without pushing it off-screen.
-		void bottom(Blittable& b); // Moves the blittable to the bottom without pushing it off-screen.
-		void left(Blittable& b); // Moves the blittable to the left without pushing off-screen.
-		void right(Blittable& b); // Moves the blittable to the right without pushing off-screen.
-
 	private:
+		int width;
+		int height;
 		bool needRefresh; // If the window needs to refresh.
 		Uint32 clearColor; // The color to clear the window with.
 		SDL_Window* window; // Window object
