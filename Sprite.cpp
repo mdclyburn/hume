a6d862a1ee446f638da46420133e6854f68c5e0c
Marshall Clyburn <mardev2013@gmail.com>
2014-02-21 01:13:43 -0500

    Adding new code for Sprite class.
    There is some sensible code now present in the Sprite class. I
    wrote this code around two and a half months ago, but I did not
    feel like merging it with Hume until now.

---
+++ Sprite.cpp
@@ -0,0 +1,133 @@
+//
+//  Sprite.cpp
+//  Game
+//
+//  Created by Marshall Clyburn on 12/30/13.
+//  Copyright (c) 2013 Marshall Clyburn. All rights reserved.
+//
+
+#include "Sprite.h"
+
+unsigned int Sprite::fps = 30;
+
+Sprite::Sprite() :  velocity(0), max_velocity(150), min_velocity(-150), acceleration(1), initial_velocity(100), state(IDLE), selection_x(0), selection_y(0)
+{
+	
+}
+
+Sprite::~Sprite()
+{
+	
+}
+
+void Sprite::act()
+{
+	switch(state)
+	{
+		case IDLE:
+			break;
+		case RUNNING_RIGHT:
+			increaseVelocity();
+			move(velocity / fps, 0);
+			break;
+		case RUNNING_LEFT:
+			decreaseVelocity();
+			move(velocity / fps, 0);
+			break;
+		case SLOWING:
+			slowVelocity(10);
+			move(velocity / fps, 0);
+			break;
+	}
+}
+
+void Sprite::setMaxVelocity(float max_velocity)
+{
+	this->max_velocity = max_velocity;
+	return;
+}
+
+void Sprite::setMinVelocity(float min_velocity)
+{
+	this->min_velocity = min_velocity;
+	return;
+}
+
+float Sprite::getVelocity()
+{
+	return velocity;
+}
+
+void Sprite::increaseVelocity()
+{
+	if(velocity >= 0) // If RUNNING_RIGHT or IDLE
+		velocity += acceleration / 30;
+	if(velocity < 0) // If RUNNING_LEFT
+		slowVelocity(15);
+	if(velocity > max_velocity) // Mustn't exceed max_acceleration
+		velocity = max_velocity;
+	return;
+}
+
+void Sprite::decreaseVelocity()
+{
+	if(velocity <= 0) // If RUNNING_LEFT or IDLE
+		velocity -= acceleration / 30;
+	if(velocity > 0) // If RUNNING_RIGHT
+		slowVelocity(25);
+	if(velocity < min_velocity) // Mustn't exceed min_acceleration
+		velocity = min_velocity;
+	return;
+}
+
+void Sprite::slowVelocity(float decel_factor)
+{
+	if(velocity == 0)
+	{
+		state = IDLE;
+		return;
+	}
+	
+	float velocity_loss = max_velocity * decel_factor / 100;
+	//std::cout << "Slowing velocity: " << velocity << " by " << velocity_loss << std::endl;
+	if(abs(int(velocity)) < abs(int(velocity_loss)))
+	   velocity = 0;
+	else if(velocity > 0)
+		velocity -= velocity_loss;
+	else if(velocity < 0)
+		velocity += velocity_loss;
+	
+	return;
+}
+
+void Sprite::setAcceleration(float acceleration)
+{
+	this->acceleration = acceleration;
+	return;
+}
+
+void Sprite::setFramesPerSecond(unsigned int fps)
+{
+	Sprite::fps = fps;
+	return;
+}
+
+void Sprite::setState(State state)
+{
+	this->state = state;
+	return;
+}
+
+State Sprite::getState()
+{
+	return state;
+}
+
+void Sprite::selectSprite(int x, int y)
+{
+	selection_x = x;
+	selection_y = y;
+	SDL_Rect r = { (x * width), (y * height), width, height };
+	input = r;
+	return;
+}
--- Sprite.h
+++ Sprite.h
@@ -1,15 +1,76 @@
-#ifndef SPRITE_H_INCLUDED
-#define SPRITE_H_INCLUDED
+//
+//  Sprite.h
+//  Game
+//
+//  Created by Marshall Clyburn on 12/30/13.
+//  Copyright (c) 2013 Marshall Clyburn. All rights reserved.
+//
+
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
 
 #include "Image.h"
 
-namespace hm
+enum State
 {
-    class Sprite : public hm::Image
-    {
-        public:
-        private:
-    };
-}
+	IDLE,
+	RUNNING_LEFT,
+	RUNNING_RIGHT,
+	SLOWING
+};
 
-#endif // SPRITE_H_INCLUDED
+class Sprite : public Image
+{
+public:
+	Sprite();
+	~Sprite();
+	
+	void act();
+	
+	void setWidth(int width);
+	void setHeight(int height);
+	
+	virtual SDL_Rect* getInputRect() = 0;
+	virtual SDL_Rect* getOutputRect() = 0;
+	
+	void setMaxVelocity(float max_velocity);
+	void setMinVelocity(float min_velocity);
+	float getVelocity();
+	void increaseVelocity();
+	void decreaseVelocity();
+	void slowVelocity(float factor);
+	void setAcceleration(float acceleration);
+	static void setFramesPerSecond(unsigned int fps);
+	
+	void setState(State state);
+	State getState();
+	
+protected:
+	int width; // Sprite dimensions
+	int height;
+	
+	float velocity; // Pixels per second.
+	float max_velocity;
+	float min_velocity;
+	float acceleration;
+	unsigned int initial_velocity;
+	static unsigned int fps;
+	
+	State state;
+	
+	int selection_x; // position (not coordinate) of selected sprite
+	int selection_y;
+	SDL_Rect input; // Position/size from texture
+	SDL_Rect output; // Position/size to window
+	
+	void selectSprite(int x, int y);
+};
+
+#endif
