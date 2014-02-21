8232c96ff3c4e68b003989e93c2297b59a798b18
Marshall Clyburn <mardev2013@gmail.com>
2014-02-21 01:20:57 -0500

    Fix new code indentation.

--- Sprite.h
+++ Sprite.h
@@ -28,52 +28,52 @@ enum State
 
 namespace hm
 {
-class Sprite : public Image
-{
-public:
-	Sprite();
-	~Sprite();
-	
-	void act();
-	
-	void setWidth(int width);
-	void setHeight(int height);
-	
-	virtual SDL_Rect* getInputRect() = 0;
-	virtual SDL_Rect* getOutputRect() = 0;
-	
-	void setMaxVelocity(float max_velocity);
-	void setMinVelocity(float min_velocity);
-	float getVelocity();
-	void increaseVelocity();
-	void decreaseVelocity();
-	void slowVelocity(float factor);
-	void setAcceleration(float acceleration);
-	static void setFramesPerSecond(unsigned int fps);
-	
-	void setState(State state);
-	State getState();
-	
-protected:
-	int width; // Sprite dimensions
-	int height;
-	
-	float velocity; // Pixels per second.
-	float max_velocity;
-	float min_velocity;
-	float acceleration;
-	unsigned int initial_velocity;
-	static unsigned int fps;
-	
-	State state;
-	
-	int selection_x; // position (not coordinate) of selected sprite
-	int selection_y;
-	SDL_Rect input; // Position/size from texture
-	SDL_Rect output; // Position/size to window
-	
-	void selectSprite(int x, int y);
-};
+	class Sprite : public Image
+	{
+	public:
+		Sprite();
+		~Sprite();
+		
+		void act();
+		
+		void setWidth(int width);
+		void setHeight(int height);
+		
+		virtual SDL_Rect* getInputRect() = 0;
+		virtual SDL_Rect* getOutputRect() = 0;
+		
+		void setMaxVelocity(float max_velocity);
+		void setMinVelocity(float min_velocity);
+		float getVelocity();
+		void increaseVelocity();
+		void decreaseVelocity();
+		void slowVelocity(float factor);
+		void setAcceleration(float acceleration);
+		static void setFramesPerSecond(unsigned int fps);
+		
+		void setState(State state);
+		State getState();
+		
+	protected:
+		int width; // Sprite dimensions
+		int height;
+		
+		float velocity; // Pixels per second.
+		float max_velocity;
+		float min_velocity;
+		float acceleration;
+		unsigned int initial_velocity;
+		static unsigned int fps;
+		
+		State state;
+		
+		int selection_x; // position (not coordinate) of selected sprite
+		int selection_y;
+		SDL_Rect input; // Position/size from texture
+		SDL_Rect output; // Position/size to window
+		
+		void selectSprite(int x, int y);
+	};
 }
 
 #endif
