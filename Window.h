3f107fb2673982ada71d18b7d762ae452936a9bb
Marshall Clyburn <mardev2013@gmail.com>
2014-07-12 16:52:06 -0400

    Remove comments from Window and WindowSettings.

--- Window.h
+++ Window.h
@@ -14,48 +14,45 @@ namespace hm
 	class Window
 	{
 	public:
-		Window(); // Defaults to best available fullscreen resolution.
+		Window();
 		Window(WindowSettings ws);
-		~Window(); // Destructor
+		~Window();
 
-		void setTitle(std::string title); // Sets the title of the window.
-		bool getStatus(); // Returns true if the window isn't null. False otherwise.
+		void setTitle(std::string title);
+		bool getStatus();
 
-		// Drawing Functions
-		void draw(Blittable& b); // Draws a Blittable object to the window's screen.
-		void draw(Blittable& b, int x, int y); // Draws a Blittable object to the window's screen, overriding the position set internally.
-		void draw(Blittable& b, int x, int y, int w, int h); // Draws a Blittable to the screen, overriding position and size.
+		void draw(Blittable& b);
+		void draw(Blittable& b, int x, int y);
+		void draw(Blittable& b, int x, int y, int w, int h);
 
-		void clear(); // Blacks out the screen.
-		void forceRefresh(); // Forces window to redraw.
-		void refresh(); // Updates the screen.
+		void clear();
+		void forceRefresh();
+		void refresh();
 
-		// Dimension Functions
-		int getWidth(); // Returns the width of the window in pixels.
-		int getHeight(); // Returns the height of the window in pixels.
+		int getWidth();
+		int getHeight();
 		
-		// Quick-Align Functions
-		void center(Blittable& b); // Centers a blittable.
-		void centerx(Blittable& b); // Centers a blittable on the X-Axis.
-		void centery(Blittable& b); // Centers a blittable on the Y-Axis.
-		void centerxco(Blittable& b, int x); // Centers a blittable along a specified X-Coordinate.
-		void centeryco(Blittable& b, int y); // Centers a blittable along a specified Y-Coordinate.
-		void centerco(Blittable& b, int x, int y); // Centers a blittable at a specified point.
-		void top(Blittable& b); // Moves the blittable to the top without pushing it off-screen.
-		void bottom(Blittable& b); // Moves the blittable to the bottom without pushing it off-screen.
-		void left(Blittable& b); // Moves the blittable to the left without pushing off-screen.
-		void right(Blittable& b); // Moves the blittable to the right without pushing off-screen.
+		void center(Blittable& b);
+		void centerx(Blittable& b);
+		void centery(Blittable& b);
+		void centerxco(Blittable& b, int x);
+		void centeryco(Blittable& b, int y);
+		void centerco(Blittable& b, int x, int y);
+		void top(Blittable& b);
+		void bottom(Blittable& b);
+		void left(Blittable& b);
+		void right(Blittable& b);
 		
 		SDL_Renderer* getRenderer();
 
 	private:
-		bool needRefresh; // If the window needs to refresh.
-		Uint32 clearColor; // The color to clear the window with.
+		bool needRefresh;
+		Uint32 clearColor;
 		WindowSettings settings;
-		SDL_Window* window; // Window object
+		SDL_Window* window;
 		SDL_Renderer* renderer;
 		
-		void create(); // Create window with current settings.
+		void create();
 	};
 }
 
