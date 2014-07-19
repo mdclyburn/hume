e7f9617d2ff83780c47b9391026c851c286a4c91
Marshall Clyburn <mardev2013@gmail.com>
2014-07-19 19:16:03 -0400

    Implement Music functions.

--- Music.cpp
+++ Music.cpp
@@ -17,26 +17,59 @@ namespace hm
 	
 	Music::~Music()
 	{
-		
+		close();
 	}
 	
 	void Music::open(std::string filename)
 	{
+		// Close music if it's open.
+		close();
 		
+		music = Mix_LoadMUS(filename.c_str());
+		if(music == nullptr)
+		{
+			Logger::log("Failed to open " + filename + ".");
+			Logger::log(Mix_GetError());
+		}
+		return;
 	}
 	
 	void Music::close()
 	{
-		
+		if(music != nullptr)
+		{
+			Mix_FreeMusic(music);
+			music = nullptr;
+		}
+		return;
 	}
 	
 	void Music::play()
 	{
+		if(music == nullptr)
+		{
+			Logger::log("Music has not been loaded.", hm::ERROR);
+			return;
+		}
 		
+		Mix_PlayMusic(music, 0);
+		return;
 	}
 	
 	void Music::setVolume(int v)
 	{
+		// Clamp values.
+		if(v < 0)
+			volume = 0;
+		else if(v > 100)
+			volume = 100;
+		else
+			volume = v;
 		
+		// Convert volume value to scale of 1 - 128.
+		float mixer_volume = ceilf(volume * 1.28);
+		hm::Logger::log("Setting chunk volume to " + std::to_string(volume) + " (" + std::to_string(int(mixer_volume)) + ")" + ".");
+		Mix_VolumeMusic(int(mixer_volume));
+		return;
 	}
 }
