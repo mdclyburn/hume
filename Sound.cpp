7eec174a096f13c5285d3fc9509d8812530d99a7
Marshall Clyburn <mardev2013@gmail.com>
2014-07-19 18:01:00 -0400

    Create empty definition of Sound class.
    This empty class will represent an single sound
    effect.

---
+++ Sound.cpp
@@ -0,0 +1,28 @@
+// Hume Library Version 0.4.3
+
+#include "Sound.h"
+
+namespace hm
+{
+	Sound::Sound() : chunk(nullptr)
+	{
+		
+	}
+	
+	Sound::~Sound()
+	{
+		
+	}
+	
+	void Sound::open(std::string filename)
+	{
+	}
+	
+	void Sound::close()
+	{
+	}
+	
+	void Sound::play()
+	{
+	}
+}
---
+++ Sound.h
@@ -0,0 +1,36 @@
+//
+//  Sound.h
+//  Hume
+//
+//  Created by Marshall Clyburn on 7/19/14.
+//  Copyright (c) 2014 Marshall Clyburn. All rights reserved.
+//
+
+#ifndef SOUND_H
+#define SOUND_H
+
+#include <string>
+
+#include "SDL2/SDL_mixer.h"
+
+#include "Logger.h"
+
+namespace hm
+{
+	class Sound
+	{
+	public:
+		Sound();
+		~Sound();
+		
+		void open(std::string filename);
+		void close();
+		
+		void play();
+		
+	protected:
+		Mix_Chunk* chunk;
+	};
+}
+
+#endif
