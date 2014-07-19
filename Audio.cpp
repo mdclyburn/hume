14c7a898ea12d710c52ddcafbeb8a75353efbcd6
Marshall Clyburn <mardev2013@gmail.com>
2014-07-19 18:31:56 -0400

    Add abstract Audio class.
    This is the class that both the Sound and Music
    classes will inherit from, although they have little
    in common when it comes to what functions they
    call to perform operations.

---
+++ Audio.cpp
@@ -0,0 +1,21 @@
+// Hume Library Version 0.4.3
+
+#include "Audio.h"
+
+namespace hm
+{
+	Audio::Audio()
+	{
+		
+	}
+	
+	Audio::~Audio()
+	{
+		
+	}
+	
+	int Audio::getVolume()
+	{
+		return volume;
+	}
+}
