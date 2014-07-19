632b48b4dbcc7378bf590460c525bd17b7a96dd0
Marshall Clyburn <mardev2013@gmail.com>
2014-07-19 18:16:52 -0400

    Implement basic Sound functions.

--- Sound.cpp
+++ Sound.cpp
@@ -11,18 +11,37 @@ namespace hm
 	
 	Sound::~Sound()
 	{
-		
+		close();
 	}
 	
 	void Sound::open(std::string filename)
 	{
+		// Close chunk if there is one loaded.
+		close();
+		
+		chunk = Mix_LoadWAV(filename.c_str());
+		if(chunk == nullptr)
+		{
+			Logger::log("Failed to load " + filename + ".");
+			Logger::log(Mix_GetError());
+		}
+		return;
 	}
 	
 	void Sound::close()
 	{
+		if(chunk != nullptr)
+		{
+			Mix_FreeChunk(chunk);
+			chunk = nullptr;
+		}
+		
+		return;
 	}
 	
 	void Sound::play()
 	{
+		Mix_PlayChannel(-1, chunk, 0);
+		return;
 	}
 }
