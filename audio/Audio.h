1fbdd01b818bb7487e5c84370028a856cfd4bda4
Marshall Clyburn <marshall@mardev.net>
2015-04-16 23:33:46 -0400

    Add audio.

---
+++ audio/Audio.h
@@ -0,0 +1,26 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <string>
+
+#include <SDL2/SDL_mixer.h>
+
+class Audio
+{
+public:
+    Audio();
+    virtual ~Audio();
+
+    virtual void open(const std::string& file_name) = 0;
+    virtual void close() = 0;
+
+    virtual void play() = 0;
+
+    virtual void set_volume(const unsigned int v) = 0;
+    virtual unsigned int get_volume() const;
+
+protected:
+    unsigned int volume;
+};
+
+#endif
