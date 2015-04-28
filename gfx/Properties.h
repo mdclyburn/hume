83d6c5e1e688c3ca54e5974208b5f7a4dece4ee4
Marshall Clyburn <marshall@mardev.net>
2015-04-27 21:57:33 -0400

    Add width and height to Properties.
    It is now possible to specify a width and height for a Blittable
    to be drawn to the screen. When any of the two values (or both)
    are set to 0, the native size of the Blittable is used.

--- gfx/Properties.h
+++ gfx/Properties.h
@@ -3,14 +3,31 @@
 #ifndef properties_h
 #define properties_h
 
+/*
+Provides options for Blittable that are to be drawn to
+the screen.
+
+x: x coordinate
+y: y coordinate
+w: width
+h: height
+
+A width or height of zero will cause the Blittable to be drawn
+using its native size.
+*/
+
 struct Properties
 {
     unsigned int x;
     unsigned int y;
+    unsigned int w;
+    unsigned int h;
 
-    Properties() : x(0), y(0) {}
+    Properties() : x(0), y(0), w(0), h(0) {}
     Properties(const unsigned int x,
-	       const unsigned int y) : x(x), y(y) {}
+	       const unsigned int y,
+	       const unsigned int w,
+	       const unsigned int h) : x(x), y(y), w(w), h(h) {}
 };
 
 #endif
