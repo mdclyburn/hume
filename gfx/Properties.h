b836a057769da396b96d3c6a1a1118060e2dd57c
Marshall Clyburn <marshall@mardev.net>
2015-05-15 19:55:43 -0400

    Introduce new variables in Properties.

--- gfx/Properties.h
+++ gfx/Properties.h
@@ -12,8 +12,17 @@ y: y coordinate
 w: width
 h: height
 
+sx: x coordinate of source in Blittable
+sy: y coordinate of source in Blittable
+sw: width of source in Blittable
+sh: height of source in Blittable
+
 A width or height of zero will cause the Blittable to be drawn
 using its native size.
+
+The s- variables allow for a specific portion of the Blittable to be
+drawn. A value of zero in the 'sw' OR 'sh' fields will cause the
+entire Blittable to be drawn.
 */
 
 struct Properties
@@ -23,11 +32,20 @@ struct Properties
     unsigned int w;
     unsigned int h;
 
+    unsigned int sx;
+    unsigned int sy;
+    unsigned int sw;
+    unsigned int sh;
+
     Properties() : x(0), y(0), w(0), h(0) {}
     Properties(const unsigned int x,
 	       const unsigned int y,
 	       const unsigned int w,
-	       const unsigned int h) : x(x), y(y), w(w), h(h) {}
+	       const unsigned int h,
+	       const unsigned int sx,
+	       const unsigned int sy,
+	       const unsigned int sw,
+	       const unsigned int sh) : x(x), y(y), w(w), h(h), sx(sx), sy(sy), sw(sw), sh(sh) {}
 };
 
 #endif
