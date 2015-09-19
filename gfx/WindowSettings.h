e51b9ed506c78c1e605033f0a15ac6dd0791474d
Marshall Clyburn <marshall@mardev.net>
2015-09-18 21:01:14 -0400

    WindowSettings documentation.

--- gfx/WindowSettings.h
+++ gfx/WindowSettings.h
@@ -1,6 +1,6 @@
 /*
 Hume Library
-Copyright (C) 2015 Marshall Clyburn
+nCopyright (C) 2015 Marshall Clyburn
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -26,16 +26,38 @@ USA
 
 #include <SDL2/SDL.h>
 
+/** Parameters for Window.
+ *
+ * Settings that can be applied to a Window.
+ */
 struct WindowSettings
 {
+	/** Whether or not to use a full screen mode. */
     bool fullscreen;
+
+	/** The text to be displayed in the title bar of the window. */
     std::string title;
+
+	/** The width of the window. */
     unsigned int width;
+
+	/** The height of the window */
     unsigned int height;
 
+	/** The default constructor.
+	 *
+	 * Defaults to a non-full screen window of size 800x450 with the title 'Hume Window'.
+	 */
     WindowSettings();
 };
 
-void set_best_fullscreen_mode();
+/** Set the best possible full screen mode.
+ *
+ * Will set the parameters to the highest possible full screen
+ * mode.
+ *
+ * \param settings the WindowSettings struct to fill
+ */
+void set_best_fullscreen_mode(WindowSettings& settings);
 
 #endif
